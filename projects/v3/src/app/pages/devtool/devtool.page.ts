intersective/app

[CORE-5977] test auth endpoint

projects/v3/src/app/pages/devtool/devtool.page.ts
@@ -56,4 +56,13 @@ export class DevtoolPage implements OnInit {
   async reviewrating() {
     this.notificationsService.popUpReviewRating(1, false);
   }
+
+  async testAuth() {
+    this.authService.authenticate({
+      email: 'learner_008@practera.com',
+      password: 'kW96dLJHrQDaaLM'
+    }).subscribe(res => {
+      console.log(res);
+    });
+  }
 }

intersective/practera-app

projects/v3/src/app/pages/devtool/devtool.page.ts
@@ -100,4 +100,13 @@ export class DevtoolPage implements OnInit {
   async reviewrating() {
     this.notificationsService.popUpReviewRating(1, false);
   }
+
+  async testAuth() {
+    this.authService.authenticate({
+      email: 'learner_008@practera.com',
+      password: 'REDACTED_TEST_PASSWORD'
+    }).subscribe(res => {
+      console.log(res);
+    });
+  }
 }