f8ad1d6cd513b74b6a9cb3246160fa7c5fd2c3ac
trtshen <cshen_581@hotmail.com>
2025-03-11 11:17:28 +0800

    [CORE-7500] milestone guideline

--- projects/v3/src/app/components/pop-up/pop-up.component.ts
+++ projects/v3/src/app/components/pop-up/pop-up.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+
+export interface PopUpData {
+  email: string;
+  message: string;
+  logo: string;
+}
 
 @Component({
   selector: 'app-pop-up',
@@ -10,12 +16,10 @@ import { ModalController } from '@ionic/angular';
 export class PopUpComponent {
   type = '';
   redirect = ['/'];
-  data: {
-    email: string;
-    message: string;
-  } = {
+  @Input() data: PopUpData = {
     email: '',
     message: '',
+    logo: '',
   };
 
   constructor(
