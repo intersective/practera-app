740edf9978a6ee74b3de14436c0cf51f6d260124
trtshen <cshen_581@hotmail.com>
2025-03-19 13:51:23 +0800

    [CORE-7500] directlink

--- projects/v3/src/app/pages/home/home.page.ts
+++ projects/v3/src/app/pages/home/home.page.ts
@@ -15,6 +15,7 @@ import { Observable, Subject } from 'rxjs';
 import { distinctUntilChanged, filter, first, takeUntil } from 'rxjs/operators';
 import { FastFeedbackService } from '@v3/app/services/fast-feedback.service';
 import { Activity } from '@v3/app/services/activity.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: "app-home",
@@ -65,6 +66,7 @@ export class HomePage implements OnInit, OnDestroy, AfterViewChecked {
     private unlockIndicatorService: UnlockIndicatorService,
     private cdr: ChangeDetectorRef,
     private fastFeedbackService: FastFeedbackService,
+    private sanitizer: DomSanitizer,
   ) {
     this.activityCount$ = homeService.activityCount$;
   }
@@ -344,30 +346,46 @@ export class HomePage implements OnInit, OnDestroy, AfterViewChecked {
   // show unlock guideline for locked milestone or activity
   async showGuideline(item: Milestone | Activity, type: 'milestone' | 'activity' = 'milestone') {
     let message = '';
+
+    const routes = [];
     const guidelines = item.unlockConditions;
+
     if (guidelines.length === 0) {
       return;
-    } else if (guidelines.length === 1) {
-      const guideline = guidelines[0];
-      message += `Please <i>${guideline.action}</i> the item <b>${guideline.name}</b> to unlock this ${type}.`;
-    } else if (guidelines.length > 1) {
+    } else if (guidelines.length >= 1) {
       message += `Please follow the steps below to unlock this ${type}:`;
+
       guidelines.forEach((guideline, index) => {
-        if (index === 0) {
-          message += '<ol>';
-        }
-        message += `<li><i>${this.utils.ucfirst(guideline.action)}</i> - <b>${guideline.name}</b></li>`;
-        if (index === guidelines.length - 1) {
-          message += '</ol>';
+        if (guideline.meta) {
+          const { activityId, assessmentId, topicId, contextId } = guideline.meta;
+
+          const action = this.utils.ucfirst(guideline.action);
+          const isMobile = this.utils.isMobile();
+          if (topicId) {
+            routes.push({
+              path: isMobile
+                ? `/v3/topic-mobile/${activityId}/${topicId}`
+                : `/v3/activity-desktop/${activityId}/${topicId}?task=topic`,
+              label: `<i><b>${action}</b></i> ${guideline.name}`,
+            });
+          } else if (assessmentId) {
+            routes.push({
+              path: isMobile
+                ? `/v3/assessment-mobile/${contextId}/${activityId}/${assessmentId}`
+                : `/v3/activity-desktop/${contextId}/${activityId}/${assessmentId}`,
+              label: `<i><b>${action}</b></i> ${guideline.name}`,
+            });
+          }
         }
       });
     }
 
     await this.notification.popUp(
-      "shortMessage",
+      "guidelines",
       {
         logo: 'lock-open',
-        message
+        message,
+        routes,
       },
     );
   }
