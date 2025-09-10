5c7cbda8feeeeb641b14bcf7814c885a4f4969b6
trtshen <cshen_581@hotmail.com>
2025-09-10 12:16:22 +0800

    [CORE-7940] pulsecheck race-condition

--- projects/v3/src/app/services/modal.service.ts
+++ projects/v3/src/app/services/modal.service.ts
@@ -7,11 +7,33 @@ import { ModalController } from '@ionic/angular';
 export class ModalService {
   private modalQueue: any[] = [];
   private isShowingModal = false;
-
+  private activeModalIds: Set<string> = new Set();
   constructor(private modalController: ModalController) { }
 
-  async addModal(modalConfig: any, callback?: Function) {
-    this.modalQueue.push({ modalConfig, callback });
+  /**
+   * Adds a modal to the queue to be displayed
+   * @param modalConfig The configuration for the modal
+   * @param callback Optional callback to execute after modal is dismissed
+   * @param modalId Optional unique identifier to prevent duplicate modals
+   * @returns Promise that resolves once the modal is added to queue
+   */
+  async addModal(modalConfig: any, callback?: Function, modalId?: string): Promise<void> {
+    // If modalId is provided, check if already in queue or being shown
+    if (modalId && this.activeModalIds.has(modalId)) {
+      console.log(`Modal with ID ${modalId} already in queue or being shown, skipping`);
+      return;
+    }
+
+    if (modalId) {
+      this.activeModalIds.add(modalId);
+    }
+
+    this.modalQueue.push({
+      modalConfig,
+      callback,
+      modalId
+    });
+
     this.showNextModal();
   }
 
@@ -26,6 +48,10 @@ export class ModalService {
     this.isShowingModal = true;
 
     modal.onDidDismiss().then(() => {
+      if (modalInfo.modalId) {
+        this.activeModalIds.delete(modalInfo.modalId);
+      }
+
       if (modalInfo.callback) {
         modalInfo.callback();
       }
