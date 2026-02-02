adbcd57b58c84a57f5fbf4f8b56ea2dfbc76af14
trtshen <cshen_581@hotmail.com>
2026-02-02 15:01:03 +0800

    [CORE-8139] assessment-specific review rating toggle

--- projects/v3/src/app/pages/activity-desktop/activity-desktop.page.ts
+++ projects/v3/src/app/pages/activity-desktop/activity-desktop.page.ts
@@ -524,7 +524,8 @@ export class ActivityDesktopPage {
     // display review rating modal
     return await this.notificationsService.popUpReviewRating(
       this.review.id,
-      false
+      false,
+      this.assessmentService.assessment?.hasReviewRating
     );
   }
 
