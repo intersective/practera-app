3433f014e28a7abdbb0dc434e87b7c1bbefb0f9b
trtshen <cshen_581@hotmail.com>
2026-03-31 13:00:44 +0800

    [CORE-8147] request module to v19

--- projects/request/src/lib/request.module.spec.ts
+++ projects/request/src/lib/request.module.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { RequestModule } from './request.module';
 
 describe('RequestModule', () => {
@@ -11,6 +13,10 @@ describe('RequestModule', () => {
           appkey: 'TEST',
           prefixUrl: 'TEST',
         }),
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
       ],
     });
 
